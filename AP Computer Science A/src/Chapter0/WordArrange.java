70184876111d88a6a9eab1bdfcf959b7e50df3ef
Trance <wuzheyuan.86@gmail.com>
2020-06-13 10:25:17 +0800

    2020.6.13

--- AP Computer Science A/src/Chapter0/WordArrange.java
+++ AP Computer Science A/src/Chapter0/WordArrange.java
@@ -5,43 +5,39 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class WordArrange {
-    private int wordLimit=100;
-    private  int[]historymap={0};
-    private int pointer=0;
+	private static int mpl = 100;
 
-    public int backToPreviousPointer(){
-        return historymap[historymap.length-2];
-    }
-
-    public int renewPointer(String waitingToCut){
-         String temp=waitingToCut.substring(pointer);
-        pointer=temp.indexOf(" ")+pointer;
-        if (pointer>wordLimit){
-            return backToPreviousPointer();
-        }
-        return renewPointer(temp);
-    }
-
-    public  String replaceString( String org, String rep){
-         String result="";
-        return result;
-    }
-
-
-    public void printParagraph(String paragraph){
-        String waitingToCut=paragraph;
-        while(waitingToCut.length()>100){
-             int newPointer= renewPointer(waitingToCut);
-            System.out.println(waitingToCut.substring(0,newPointer));
-            waitingToCut=waitingToCut.substring(newPointer);
-        }
-        System.out.println(waitingToCut);
-    }
-
-    public void printThesis(List <String> thesis){
-        for (String paraghragh:thesis){
-            printParagraph(paraghragh);
-        }
-    }
-
+	public static void main(String[] args) {
+		//get input 
+		Scanner a = new Scanner(System.in);
+		String userInput = a.nextLine();
+		//cut the input into several words
+		String[] character = userInput.split(" ");
+		//the index in character 
+		int index=0;
+		//when there is words that didn't put in
+		while (index<character.length) {
+			//create a list
+			List<String> c = new ArrayList<String>();
+			//current word count 
+			int cwc=0;
+			//when there length of the string is less than 100
+			while(cwc+character[index].length()+1<mpl) {
+				//add the character into print list
+				c.add(character[index]);
+				//add current wordCount
+				cwc=cwc+character[index].length()+1;
+				//add index
+				index++;
+				if (index==character.length) {
+					break;
+				}
+			}
+			//print the words
+			for (String i :c) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
 }
